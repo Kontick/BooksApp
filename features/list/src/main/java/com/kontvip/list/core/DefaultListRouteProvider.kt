4ae2b3ad7ee616d284fb876173a0f19f472f8acb
Ivan Titov <titov.i@tytocare.com>
2024-06-16 17:16:54 +0200

    Code refactoring.

--- features/list/src/main/java/com/kontvip/list/core/DefaultListRouteProvider.kt
+++ features/list/src/main/java/com/kontvip/list/core/DefaultListRouteProvider.kt
@@ -4,5 +4,5 @@ import com.kontvip.common.navigation.Route
 import com.kontvip.common.navigation.ListRouteProvider
 
 class DefaultListRouteProvider : ListRouteProvider {
-    override fun route(): Route = ListRoute()
+    override fun route(): Route = ListRoute
 }
